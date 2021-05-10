77d1a3a3ab1175c4d194a61d0710756b6d9dac74
YuriiTrofimenko <tyaa@ukr.net>
2021-05-10 19:56:38 +0300

    init n demo

---
+++ app/src/main/java/org/tyaa/demo/java/android/oneactivitytwofragments/MainActivity.java
@@ -0,0 +1,56 @@
+package org.tyaa.demo.java.android.oneactivitytwofragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragmentTop = new TopFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.topLayout, fragmentTop, fragmentTop.getTag())
+                .commit();
+
+        Fragment fragmentBottom = new BottomFragment();
+        fragmentManager.beginTransaction()
+                .replace(R.id.bottomLayout, fragmentBottom, fragmentBottom.getTag())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
